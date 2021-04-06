84f95894ff277e3635453ac8f016adc0d9520a96
mariamrtz00 <m.martinezru.2018@alumnos.urjc.es>
2021-04-06 14:08:07 +0200

    1. CREO CANVAS

---
+++ P3/S6/canvas-01.js
@@ -0,0 +1,7 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas"); // accedo al doc html y al alemento con id 'canvas'
+
+//-- Definir el tamaño del convas. DOY TAMAÑO AL CANVAS AQUÍ EN LUGAR DE EN EL HTML
+canvas.width = 300;
+canvas.height = 100;
