2087a917e358eb864dfef2bd7faf6c3de338c02d
mariamrtz00 <m.martinezru.2018@alumnos.urjc.es>
2021-04-06 17:45:46 +0200

    Ejemplo 9: Movimiento horizontal de un rectángulo

---
+++ P3/S6/anim-02.js
@@ -0,0 +1,52 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del canvas
+canvas.width = 300;
+canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Coordenadas del objeto
+let x = 0;
+let y = 10; // aahora el rectángulo empieza en (0,10)
+
+//-- Velocidad horizontal del objeto
+let velx = 1; // como quiero que se mueva, le doy una velocidad, se moverá en el eje horizontal
+// si quisiese que cuando el rectángulo llegase al final del canvas volviese a la posición 0 tendría que hacer un if
+
+//-- Funcion principal de animacion
+function update() 
+{
+  console.log("test");
+  //-- Algoritmo de animacion:
+  //-- 1) Actualizar posicion del  elemento
+  //-- (física del movimiento rectilineo uniforme)
+  x = x + velx; // fisica del M RECT UNIFORME
+  // cuanto más velocidad, más se incrementa la posición ( en cada frame se hace este calculo)
+
+  //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+    ctx.rect(x, y, 20, 20);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update(); 
