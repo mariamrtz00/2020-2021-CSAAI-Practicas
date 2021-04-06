b02a34380cacdbb9ebb47723854cc3cd4ab9acf5
mariamrtz00 <m.martinezru.2018@alumnos.urjc.es>
2021-04-06 14:29:05 +0200

    4. dibujar líneas

---
+++ P3/S6/canvas-04.js
@@ -0,0 +1,30 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del convas
+canvas.width = 200;
+canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+
+ctx.beginPath();
+    //-- Línea horizontal
+    ctx.moveTo(10, 20); // moveto no dibuja, solo me situa en esa coordenada x=10, y=20
+    ctx.lineTo(100, 20); // me muevo a la nueva posición, pero dibujando: x irá de 10 a 100 
+
+    //-- Línea horizontal y vertical, unidas
+    ctx.moveTo(10, 80);
+    ctx.lineTo(150,80);
+    ctx.lineTo(150,20);
+
+    ctx.strokeStyle = 'blue';
+    //-- Cambiar el tamaño de la linea del trazo
+    ctx.lineWidth = 4;
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+    
+ctx.closePath()
