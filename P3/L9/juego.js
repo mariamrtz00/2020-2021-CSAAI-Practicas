f5697bf4065a57eb0f2b8798852b6a920d743aba
mariamrtz00 <m.martinezru.2018@alumnos.urjc.es>
2021-05-04 01:49:20 +0200

    poco a poco esto va para largo

--- P3/L9/juego.js
+++ P3/L9/juego.js
@@ -1,5 +1,6 @@
 console.log("Ejecutando JS...");
 const canvas = document.getElementById("canvas");
+
 //-- Definir el tamaño del canvas
 canvas.width = 255; // ancho
 canvas.height = 400;
@@ -19,166 +20,6 @@ let velx = 1; // horiz
 let vely = 1; // verti
 let vidas_dispo = 3;
 
-
-
-
-//-- Funcion principal de animacion
-// 1. Actualiza la posicion del elemento
-// 2. Borrar el canvas
-// 3. Pintaar en el canvas
-// se repite el proceso
-function update() {
-    console.log("test");
-  
-    // para que rebote en las paredes
-    if (x < 0 || x >= (canvas.width - 20) ) {
-        velx = -velx;
-    } 
-    if (y < 0) {
-        vely = -vely;
-
-    }
-    //if (y> canvas.height - 20) {
-      //  x= xraq;
-        //y= yraq;
-    //}
-  
-
-    //-- Actualizar la posición
-    x = x + velx;
-    y = y - vely; // PARA QUE LA BOLA VAYA HACIA ARRIBA
-
-    //-- 2) Borrar el canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-   //-- 3) A pintarrrr
-
-    ctx.beginPath();
-    //-- Definir un rectangulo de dimensiones 100x50,
-    //-- cuya esquina superior izquierda está en (5,5)
-        ctx.rect(5,5, 20, 10);
-        ctx.rect(30,5, 20, 10);
-        ctx.rect(55,5, 20, 10);
-        ctx.rect(80,5, 20, 10);
-        ctx.rect(105,5, 20, 10);
-        ctx.rect(130,5, 20, 10);
-        ctx.rect(155,5, 20, 10); 
-        ctx.rect(180,5, 20, 10);
-        ctx.rect(205,5, 20, 10); 
-        ctx.rect(230,5, 20, 10);
-
-        ctx.rect(5,20, 20, 10);
-        ctx.rect(30,20, 20, 10);
-        ctx.rect(55,20, 20, 10);
-        ctx.rect(80,20, 20, 10);
-        ctx.rect(105,20, 20, 10);
-        ctx.rect(130,20, 20, 10);
-        ctx.rect(155,20, 20, 10);
-        ctx.rect(180,20, 20, 10);
-        ctx.rect(205,20, 20, 10); 
-        ctx.rect(230,20, 20, 10);
-
-        ctx.rect(5,35, 20, 10);
-        ctx.rect(30,35, 20, 10);
-        ctx.rect(55,35, 20, 10);
-        ctx.rect(80,35, 20, 10);
-        ctx.rect(105,35, 20, 10);
-        ctx.rect(130,35, 20, 10);
-        ctx.rect(155,35, 20, 10);
-        ctx.rect(180,35, 20, 10);
-        ctx.rect(205,35, 20, 10); 
-        ctx.rect(230,35, 20, 10);
-
-        ctx.rect(5,50, 20, 10);
-        ctx.rect(30,50, 20, 10);
-        ctx.rect(55,50, 20, 10);
-        ctx.rect(80,50, 20, 10);
-        ctx.rect(105,50, 20, 10);
-        ctx.rect(130,50, 20, 10);
-        ctx.rect(155,50, 20, 10);
-        ctx.rect(180,50, 20, 10);
-        ctx.rect(205,50, 20, 10); 
-        ctx.rect(230,50, 20, 10);
-
-        ctx.rect(5,65, 20, 10);
-        ctx.rect(30,65, 20, 10);
-        ctx.rect(55,65, 20, 10);
-        ctx.rect(80,65, 20, 10);
-        ctx.rect(105,65, 20, 10);
-        ctx.rect(130,65, 20, 10);
-        ctx.rect(155,65, 20, 10);
-        ctx.rect(180,65, 20, 10);
-        ctx.rect(205,65, 20, 10); 
-        ctx.rect(230,65, 20, 10);
-
-        ctx.rect(5,80, 20, 10);
-        ctx.rect(30,80, 20, 10);
-        ctx.rect(55,80, 20, 10);
-        ctx.rect(80,80, 20, 10);
-        ctx.rect(105,80, 20, 10);
-        ctx.rect(130,80, 20, 10);
-        ctx.rect(155,80, 20, 10);
-        ctx.rect(180,80, 20, 10);
-        ctx.rect(205,80, 20, 10); 
-        ctx.rect(230,80, 20, 10);
-
-        ctx.rect(5,95, 20, 10);
-        ctx.rect(30,95, 20, 10);
-        ctx.rect(55,95, 20, 10);
-        ctx.rect(80,95, 20, 10);
-        ctx.rect(105,95, 20, 10);
-        ctx.rect(130,95, 20, 10);
-        ctx.rect(155,95, 20, 10);
-        ctx.rect(180,95, 20, 10);
-        ctx.rect(205,95, 20, 10); 
-        ctx.rect(230,95, 20, 10);
-        ctx.fillStyle = 'pink'
-        ctx.strokeStyle = 'violet'; 
-        ctx.lineWidth = 2.0; 
-        //-- Mostrar el relleno
-        ctx.fill();
-        //-- Mostrar el trazo del rectángulo
-        ctx.stroke();
-    //ctx.closePath();
-
-    ctx.beginPath();
-    
-        ctx.rect(xraq,yraq, 40, 7); // base para que rebote la pelota
-        // x= 70(donde empieza a dibujarse), y= 350 (el 0,0 está arriba a la izquierda)
-        // grosor =7, largo=40
-
-        //-- Color de relleno del rectángulo
-        ctx.fillStyle = 'black';
-        ctx.strokeStyle = 'yellow'; 
-        ctx.lineWidth = 0.2; 
-        //-- Mostrar el relleno
-        ctx.fill();
-
-        //-- Mostrar el trazo del rectángulo
-        ctx.stroke();
-    //ctx.closePath();
-
-    ctx.beginPath();
-    // pelotita
-    // FALLO: NO ESTABA PONIENDO X E Y ENTONCES NO SE ACTUALIZA LA PELOTA CON LAS POSICIONES
-
-        ctx.arc(x, y, 7, 0, 2 * Math.PI); 
-        // radio = 7
-
-        ctx.fillStyle = 'blue';
-        
-        ctx.fill();
-    ctx.closePath();
-
-
-  //-- 4) Volver a ejecutar update cuando toque
-  requestAnimationFrame(update);
-}
-
-
-update();
-
-
 pausa.onclick = () => {
   coordenadas = [];
   coordenadas.push(x);
@@ -189,25 +30,32 @@ pausa.onclick = () => {
   vely = 0;
 }
 
-cont.onclick = () => {
-  x = coordenadas[0];
-  y = coordenadas[1];
-  velx = coordenadas[2];
-  vely = coordenadas[3];
-  x = x + velx;
-  y = y + vely;
-  
+// a poner los ladrillos sin tener mil líneas de código
+const config_ladrillos = {
+  filas: 6, 
+  columnas: 10, 
+  ancho: 30, 
+  alto: 15, 
+  padding: 10, 
+  visible: true 
 }
 
+const ladrillos = []; 
 
-body = document.getElementsByTagName('body')[0]
+for (let i =0; i < config_ladrillos.filas; i++){ // recorre numero de filas
+    ladrillos[i] = [];  // filas
+    for (let j =0; j < config_ladrillos.columnas; j++){ // recorre numero de columnas de cada fila
 
-//-- Funcion de retrollamada de tecla pulsada
-window.onkeydown = (e) => {
-  
-  //-- Comprobar si la tecla es un espacio
-  if (e.key == ' ') {
+      // calculamos valores para los ladrillos 
+        ladrillos[i][j] = { // filas y columnas
 
-    //-- Cambiar la activación de la clase color
-    body.classList.toggle("color");
-  }}
+            x: 25 + (config_ladrillos.ancho + config_ladrillos.padding) * j,
+            y: 75 + (config_ladrillos.alto + config_ladrillos.padding) * i,
+            w: config_ladrillos.ancho,
+            h: config_ladrillos.alto,
+            padding: config_ladrillos.padding,
+            visible: config_ladrillos.visible
+
+        };
+    }
+}
