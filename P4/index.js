c818dded49fa80d7147e7d5ae5d02e68a3c425cc
mariamrtz00 <m.martinezru.2018@alumnos.urjc.es>
2021-05-09 19:58:37 +0200

    Deberia hacer la imagen como 8 veces más pequeña

--- P4/index.js
+++ P4/index.js
@@ -6,10 +6,14 @@ const img = document.getElementById('imagesrc');
 const ctx = canvas.getContext('2d');
 
 //-- Acceso al deslizador
-const deslizador = document.getElementById('deslizador');
+const deslizadorR = document.getElementById('Rdeslizador');
+const deslizadorG = document.getElementById('Gdeslizador');
+const deslizadorB = document.getElementById('Bdeslizador');
 
 //-- Valor del deslizador
-const range_value = document.getElementById('range_value');
+const range_valueR = document.getElementById('range_value_r');
+const range_valueG = document.getElementById('range_value_g');
+const range_valueB = document.getElementById('range_value_b');
 
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
@@ -30,32 +34,48 @@ img.onload = function () {
 };
 
 
-//-- Funcion de retrollamada del deslizador
-deslizador.oninput = () => { // en cuanto el slider cambie
-  //-- Mostrar el nuevo valor del deslizador
-  range_value.innerHTML = deslizador.value; // el valor actual del slider se tiene que mostrar pa saber lo que estamos aplicando
+deslizadorR.oninput = () => {// en cuanto el slider cambie
+    colores();
+  }
+  
+  deslizadorG.oninput = () => {
+    colores();
+  }
+  
+  deslizadorB.oninput = () => {
+    colores();
+  }
 
-  //-- Situar la imagen original en el canvas
-  //-- No se han hecho manipulaciones todavia
-  ctx.drawImage(img, 0,0); // en este caso partimos de la misma imagen original, podríamos aplicar filtros sonbre imgane ya con un filtro
+//-- Funcion de retrollamada del deslizador  
+function colores(){
+    //-- Mostrar el nuevo valor del deslizador
+    range_value.innerHTML = deslizador.value; // el valor actual del slider se tiene que mostrar pa saber lo que estamos aplicando
 
-  //-- Obtener la imagen del canvas en pixeles
-  let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    //-- Situar la imagen original en el canvas
+    //-- No se han hecho manipulaciones todavia
+    ctx.drawImage(img, 0,0); // en este caso partimos de la misma imagen original, podríamos aplicar filtros sonbre imgane ya con un filtro
 
-  //-- Obtener el array con todos los píxeles
-  let data = imgData.data
+    //-- Obtener la imagen del canvas en pixeles
+    let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-  //-- Obtener el umbral de rojo del desliador
-  umbral = deslizador.value
+    //-- Obtener el array con todos los píxeles
+    let data = imgData.data
 
-  //-- Filtrar la imagen según el nuevo umbral
-  for (let i = 0; i < data.length; i+=4) { // solo quiero las componentes rojas por eso voy de 4 en 4
+    //-- Obtener el umbral de rojo del desliador
+    umbral1 = deslizadorR.value
+    umbral2 = deslizadorG.value
+    umbral3 = deslizadorB.value
+
+    //-- Filtrar la imagen según el nuevo umbral
+    for (let i = 0; i < data.length; i+=4) { // solo quiero las componentes rojas por eso voy de 4 en 4
     if (data[i] > umbral) // si la compo roja del pixel actual es > del umbral que he metido
-      data[i] = umbral; // me lo baja al umbral que le he puesto
-  } // y si no, se queda como estaba
+        data[i] = umbral; // me lo baja al umbral que le he puesto
+    } // y si no, se queda como estaba
 
-  //-- Poner la imagen modificada en el canvas
-  ctx.putImageData(imgData, 0, 0);
+    //-- Poner la imagen modificada en el canvas
+    ctx.putImageData(imgData, 0, 0);
+
 }
+
 
 console.log("Fin...");
