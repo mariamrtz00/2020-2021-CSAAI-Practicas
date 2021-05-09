1d96773b1aebd05db099175c8134a86972d0ffe5
mariamrtz00 <m.martinezru.2018@alumnos.urjc.es>
2021-05-09 20:28:43 +0200

    sliders okkkkkkkkkkkkkkk

--- P4/index.js
+++ P4/index.js
@@ -15,6 +15,12 @@ const range_valueR = document.getElementById('range_value_r');
 const range_valueG = document.getElementById('range_value_g');
 const range_valueB = document.getElementById('range_value_b');
 
+//-- Botones
+const btn_gris = document.getElementById('grises');
+const btn_origi = document.getElementById('original');
+
+var grises = false; // de primeras la imagene está normal 
+
 //-- Función de retrollamada de imagen cargada
 //-- La imagen no se carga instantaneamente, sino que
 //-- lleva un tiempo. Sólo podemos acceder a ella una vez
@@ -36,24 +42,56 @@ img.onload = function () {
 
 deslizadorR.oninput = () => {// en cuanto el slider cambie
     colores();
+    console.log("Deslizador rojo");
   }
   
-  deslizadorG.oninput = () => {
+deslizadorG.oninput = () => {
     colores();
   }
   
-  deslizadorB.oninput = () => {
+deslizadorB.oninput = () => {
     colores();
   }
 
 //-- Funcion de retrollamada del deslizador  
 function colores(){
     //-- Mostrar el nuevo valor del deslizador
-    range_value.innerHTML = deslizador.value; // el valor actual del slider se tiene que mostrar pa saber lo que estamos aplicando
+    if (grises== false){
+        range_valueR.innerHTML = deslizadorR.value; // el valor actual del slider se tiene que mostrar pa saber lo que estamos aplicando
+        range_valueG.innerHTML = deslizadorG.value;
+        range_valueB.innerHTML = deslizadorB.value;
+        //-- Situar la imagen original en el canvas
+        //-- No se han hecho manipulaciones todavia
+        ctx.drawImage(img, 0,0); // en este caso partimos de la misma imagen original, podríamos aplicar filtros sonbre imgane ya con un filtro
 
-    //-- Situar la imagen original en el canvas
-    //-- No se han hecho manipulaciones todavia
-    ctx.drawImage(img, 0,0); // en este caso partimos de la misma imagen original, podríamos aplicar filtros sonbre imgane ya con un filtro
+        //-- Obtener la imagen del canvas en pixeles
+        let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+        //-- Obtener el array con todos los píxeles
+        let data = imgData.data
+
+        //-- Obtener el umbral de rojo del desliador
+        umbral1 = deslizadorR.value
+        umbral2 = deslizadorG.value
+        umbral3 = deslizadorB.value
+
+        //-- Filtrar la imagen según el nuevo umbral
+        for (let i = 0; i < data.length; i+=4) { // solo quiero las componentes rojas por eso voy de 4 en 4
+        if (data[i] > umbral1){ // si la compo roja del pixel actual es > del umbral que he metido
+            data[i] = umbral1;} // me lo baja al umbral que le he puesto
+        if (data[i + 1] > umbral2){
+            data[i + 1] = umbral2;}
+        if (data[i + 2] > umbral3){
+            data[i + 2] = umbral3;}
+        } // y si no, se queda como estaba
+
+        //-- Poner la imagen modificada en el canvas
+        ctx.putImageData(imgData, 0, 0);
+    }
+}
+
+btn_gris.onclick = () =>{
+    grises = true;
 
     //-- Obtener la imagen del canvas en pixeles
     let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
@@ -61,21 +99,8 @@ function colores(){
     //-- Obtener el array con todos los píxeles
     let data = imgData.data
 
-    //-- Obtener el umbral de rojo del desliador
-    umbral1 = deslizadorR.value
-    umbral2 = deslizadorG.value
-    umbral3 = deslizadorB.value
-
-    //-- Filtrar la imagen según el nuevo umbral
-    for (let i = 0; i < data.length; i+=4) { // solo quiero las componentes rojas por eso voy de 4 en 4
-    if (data[i] > umbral) // si la compo roja del pixel actual es > del umbral que he metido
-        data[i] = umbral; // me lo baja al umbral que le he puesto
-    } // y si no, se queda como estaba
-
     //-- Poner la imagen modificada en el canvas
     ctx.putImageData(imgData, 0, 0);
-
 }
-
 
 console.log("Fin...");
